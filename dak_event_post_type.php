a9ae10c097e63bed0e06c3317dc5a2baad5e1327
Snorre Magnus Davøen <snorremd@gmail.com>
2013-02-02 17:17:52 +0100

    Removed a logging statement from xmlrpc module.

--- dak_event_post_type.php
+++ dak_event_post_type.php
@@ -1,16 +1,5 @@
 <?php
-/*
-Plugin Name: Event Post Type
-Description: Add penguins to your smugmug album posts!
-Author: Snorre Davøen, Lisa Halvorsen, Robin Garen Aaberg.
-Version: 0.0003
-*/
-
-require_once("dak_event_xmlrpc.php");
-
-$post_type_namespace = "dak_event";// Set up hooks
-add_action('init', 'dak_event_create_post_type');
-//add_action('add_meta_boxes', 'dak_add_meta_boxes');
+$post_type_namespace = "dak_event";
 
 function dak_event_create_post_type() {
     global $post_type_namespace;
@@ -36,13 +25,12 @@ function dak_event_create_post_type() {
             'register_meta_box_cb' => 'dak_event_add_metaboxes'
         )
     );
-
-    // Adding xml-rpc methods
-    add_filter( 'xmlrpc_methods', dak_event_add_xmlrpc_methods);
 }
 
 /* Adds a box to the main column on the Post and Page edit screens */
 function dak_event_add_metaboxes() { 
+    global $post_type_namespace;
+
     add_meta_box( 
         "dak_event_id",
         __("id"), "dak_event_id",
@@ -128,6 +116,24 @@ function dak_event_add_metaboxes() {
     );
 
     add_meta_box( 
+        "dak_event_arranger_name",
+        __("arranger_name"), "dak_event_arranger_name",
+        $post_type_namespace
+    );
+
+    add_meta_box( 
+        "dak_event_arranger_logo",
+        __("arranger_logo"), "dak_event_arranger_logo",
+        $post_type_namespace
+    );
+
+    add_meta_box( 
+        "dak_event_arranger_description",
+        __("arranger_description"), "dak_event_arranger_description",
+        $post_type_namespace
+    );
+
+    add_meta_box( 
         "dak_event_festival_id",
         __("festival_id"), "dak_event_festival_id",
         $post_type_namespace
@@ -182,10 +188,11 @@ function dak_event_add_metaboxes() {
     );
 }
 
+
 function dak_event_id() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_id, true);
+    $meta = get_post_meta($post->ID, 'dak_event_id', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_id" value="'.$meta.'" />';
    
@@ -194,7 +201,7 @@ function dak_event_id() {
 function dak_event_url() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_url, true);
+    $meta = get_post_meta($post->ID, 'dak_event_url', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_url" value="'.$meta.'" />';
    
@@ -203,7 +210,7 @@ function dak_event_url() {
 function dak_event_ical() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_ical, true);
+    $meta = get_post_meta($post->ID, 'dak_event_ical', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_ical" value="'.$meta.'" />';
    
@@ -212,7 +219,7 @@ function dak_event_ical() {
 function dak_event_linkout() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_linkout, true);
+    $meta = get_post_meta($post->ID, 'dak_event_linkout', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_linkout" value="'.$meta.'" />';
    
@@ -221,7 +228,7 @@ function dak_event_linkout() {
 function dak_event_start_date() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_start_date, true);
+    $meta = get_post_meta($post->ID, 'dak_event_start_date', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_start_date" value="'.$meta.'" />';
    
@@ -230,7 +237,7 @@ function dak_event_start_date() {
 function dak_event_start_time() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_start_time, true);
+    $meta = get_post_meta($post->ID, 'dak_event_start_time', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_start_time" value="'.$meta.'" />';
    
@@ -239,7 +246,7 @@ function dak_event_start_time() {
 function dak_event_end_date() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_end_date, true);
+    $meta = get_post_meta($post->ID, 'dak_event_end_date', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_end_date" value="'.$meta.'" />';
    
@@ -248,7 +255,7 @@ function dak_event_end_date() {
 function dak_event_end_time() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_end_time, true);
+    $meta = get_post_meta($post->ID, 'dak_event_end_time', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_end_time" value="'.$meta.'" />';
    
@@ -257,7 +264,7 @@ function dak_event_end_time() {
 function dak_event_is_accepted() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_is_accepted, true);
+    $meta = get_post_meta($post->ID, 'dak_event_is_accepted', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_is_accepted" value="'.$meta.'" />';
    
@@ -266,7 +273,7 @@ function dak_event_is_accepted() {
 function dak_event_is_public() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_is_public, true);
+    $meta = get_post_meta($post->ID, 'dak_event_is_public', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_is_public" value="'.$meta.'" />';
    
@@ -275,7 +282,7 @@ function dak_event_is_public() {
 function dak_event_custom_location() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_custom_location, true);
+    $meta = get_post_meta($post->ID, 'dak_event_custom_location', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_custom_location" value="'.$meta.'" />';
    
@@ -284,7 +291,7 @@ function dak_event_custom_location() {
 function dak_event_common_location() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_common_location, true);
+    $meta = get_post_meta($post->ID, 'dak_event_common_location', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_common_location" value="'.$meta.'" />';
    
@@ -293,7 +300,7 @@ function dak_event_common_location() {
 function dak_event_location_id() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_location_id, true);
+    $meta = get_post_meta($post->ID, 'dak_event_location_id', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_location_id" value="'.$meta.'" />';
    
@@ -302,16 +309,43 @@ function dak_event_location_id() {
 function dak_event_arranger_id() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_arranger_id, true);
+    $meta = get_post_meta($post->ID, 'dak_event_arranger_id', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_arranger_id" value="'.$meta.'" />';
+   
+}
+
+function dak_event_arranger_name() {
+    global $post;
+    $nonce = wp_create_nonce( plugin_basename(__FILE__) );
+    $meta = get_post_meta($post->ID, 'dak_event_arranger_name', true);
+    echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
+    echo '<input type="text" name="dak_event_arranger_name" value="'.$meta.'" />';
+   
+}
+
+function dak_event_arranger_logo() {
+    global $post;
+    $nonce = wp_create_nonce( plugin_basename(__FILE__) );
+    $meta = get_post_meta($post->ID, 'dak_event_arranger_logo', true);
+    echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
+    echo '<input type="text" name="dak_event_arranger_logo" value="'.$meta.'" />';
+   
+}
+
+function dak_event_arranger_description() {
+    global $post;
+    $nonce = wp_create_nonce( plugin_basename(__FILE__) );
+    $meta = get_post_meta($post->ID, 'dak_event_arranger_description', true);
+    echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
+    echo '<input type="text" name="dak_event_arranger_description" value="'.$meta.'" />';
    
 }
 
 function dak_event_festival_id() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_festival_id, true);
+    $meta = get_post_meta($post->ID, 'dak_event_festival_id', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_festival_id" value="'.$meta.'" />';
    
@@ -320,7 +354,7 @@ function dak_event_festival_id() {
 function dak_event_primary_picture() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_primary_picture, true);
+    $meta = get_post_meta($post->ID, 'dak_event_primary_picture', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_primary_picture" value="'.$meta.'" />';
    
@@ -329,7 +363,7 @@ function dak_event_primary_picture() {
 function dak_event_covercharge() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_covercharge, true);
+    $meta = get_post_meta($post->ID, 'dak_event_covercharge', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_covercharge" value="'.$meta.'" />';
    
@@ -338,7 +372,7 @@ function dak_event_covercharge() {
 function dak_event_age_limit() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_age_limit, true);
+    $meta = get_post_meta($post->ID, 'dak_event_age_limit', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_age_limit" value="'.$meta.'" />';
    
@@ -347,7 +381,7 @@ function dak_event_age_limit() {
 function dak_event_created_at() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_created_at, true);
+    $meta = get_post_meta($post->ID, 'dak_event_created_at', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_created_at" value="'.$meta.'" />';
    
@@ -356,7 +390,7 @@ function dak_event_created_at() {
 function dak_event_updated_at() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_updated_at, true);
+    $meta = get_post_meta($post->ID, 'dak_event_updated_at', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_updated_at" value="'.$meta.'" />';
    
@@ -365,7 +399,7 @@ function dak_event_updated_at() {
 function dak_event_arranger() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_arranger, true);
+    $meta = get_post_meta($post->ID, 'dak_event_arranger', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_arranger" value="'.$meta.'" />';
    
@@ -374,7 +408,7 @@ function dak_event_arranger() {
 function dak_event_categories() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_categories, true);
+    $meta = get_post_meta($post->ID, 'dak_event_categories', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_categories" value="'.$meta.'" />';
    
@@ -383,47 +417,53 @@ function dak_event_categories() {
 function dak_event_festival() {
     global $post;
     $nonce = wp_create_nonce( plugin_basename(__FILE__) );
-    $meta = get_post_meta($post->ID, dak_event_festival, true);
+    $meta = get_post_meta($post->ID, 'dak_event_festival', true);
     echo '<input type="hidden" name="meta_noncename" value="'.$nonce.'" />';
     echo '<input type="text" name="dak_event_festival" value="'.$meta.'" />';
    
 }
 
+
 // Method hijacked from Devin @ http://wptheming.com/2010/08/custom-metabox-for-post-type/ 
 function dak_event_save_post_meta($post_id, $post) {
     // verify this came from the our screen and with proper authorization,
     // because save_post can be triggered at other times
-    if ( !wp_verify_nonce( $_POST['meta_noncename'], plugin_basename(__FILE__) )) {
-    return $post->ID;
+    
+    if (!empty($_POST['meta_noncename']) && !wp_verify_nonce( $_POST['meta_noncename'], plugin_basename(__FILE__) )) {
+        return $post->ID;
     }
     // Is the user allowed to edit the post or page?
     if ( !current_user_can( 'edit_post', $post->ID ))
         return $post->ID;
     // OK, we're authenticated: we need to find and save the data
     // We'll put it into an array to make it easier to loop though.
-	$dak_event_meta['dak_event_id'] = $_POST['dak_event_id'];
-	$dak_event_meta['dak_event_url'] = $_POST['dak_event_url'];
-	$dak_event_meta['dak_event_ical'] = $_POST['dak_event_ical'];
-	$dak_event_meta['dak_event_linkout'] = $_POST['dak_event_linkout'];
-	$dak_event_meta['dak_event_start_date'] = $_POST['dak_event_start_date'];
-	$dak_event_meta['dak_event_start_time'] = $_POST['dak_event_start_time'];
-	$dak_event_meta['dak_event_end_date'] = $_POST['dak_event_end_date'];
-	$dak_event_meta['dak_event_end_time'] = $_POST['dak_event_end_time'];
-	$dak_event_meta['dak_event_is_accepted'] = $_POST['dak_event_is_accepted'];
-	$dak_event_meta['dak_event_is_public'] = $_POST['dak_event_is_public'];
-	$dak_event_meta['dak_event_custom_location'] = $_POST['dak_event_custom_location'];
-	$dak_event_meta['dak_event_common_location'] = $_POST['dak_event_common_location'];
-	$dak_event_meta['dak_event_location_id'] = $_POST['dak_event_location_id'];
-	$dak_event_meta['dak_event_arranger_id'] = $_POST['dak_event_arranger_id'];
-	$dak_event_meta['dak_event_festival_id'] = $_POST['dak_event_festival_id'];
-	$dak_event_meta['dak_event_primary_picture'] = $_POST['dak_event_primary_picture'];
-	$dak_event_meta['dak_event_covercharge'] = $_POST['dak_event_covercharge'];
-	$dak_event_meta['dak_event_age_limit'] = $_POST['dak_event_age_limit'];
-	$dak_event_meta['dak_event_created_at'] = $_POST['dak_event_created_at'];
-	$dak_event_meta['dak_event_updated_at'] = $_POST['dak_event_updated_at'];
-	$dak_event_meta['dak_event_arranger'] = $_POST['dak_event_arranger'];
-	$dak_event_meta['dak_event_categories'] = $_POST['dak_event_categories'];
-	$dak_event_meta['dak_event_festival'] = $_POST['dak_event_festival'];
+    $dak_event_meta = array();
+    if(!empty($_POST['dak_event_id'])) $dak_event_meta['dak_event_id'] = $_POST['dak_event_id'];
+    if(!empty($_POST['dak_event_url'])) $dak_event_meta['dak_event_url'] = $_POST['dak_event_url'];
+    if(!empty($_POST['dak_event_ical'])) $dak_event_meta['dak_event_ical'] = $_POST['dak_event_ical'];
+    if(!empty($_POST['dak_event_linkout'])) $dak_event_meta['dak_event_linkout'] = $_POST['dak_event_linkout'];
+    if(!empty($_POST['dak_event_start_date'])) $dak_event_meta['dak_event_start_date'] = $_POST['dak_event_start_date'];
+    if(!empty($_POST['dak_event_start_time'])) $dak_event_meta['dak_event_start_time'] = $_POST['dak_event_start_time'];
+    if(!empty($_POST['dak_event_end_date'])) $dak_event_meta['dak_event_end_date'] = $_POST['dak_event_end_date'];
+    if(!empty($_POST['dak_event_end_time'])) $dak_event_meta['dak_event_end_time'] = $_POST['dak_event_end_time'];
+    if(!empty($_POST['dak_event_is_accepted'])) $dak_event_meta['dak_event_is_accepted'] = $_POST['dak_event_is_accepted'];
+    if(!empty($_POST['dak_event_is_public'])) $dak_event_meta['dak_event_is_public'] = $_POST['dak_event_is_public'];
+    if(!empty($_POST['dak_event_custom_location'])) $dak_event_meta['dak_event_custom_location'] = $_POST['dak_event_custom_location'];
+    if(!empty($_POST['dak_event_common_location'])) $dak_event_meta['dak_event_common_location'] = $_POST['dak_event_common_location'];
+    if(!empty($_POST['dak_event_location_id'])) $dak_event_meta['dak_event_location_id'] = $_POST['dak_event_location_id'];
+    if(!empty($_POST['dak_event_arranger_id'])) $dak_event_meta['dak_event_arranger_id'] = $_POST['dak_event_arranger_id'];
+    if(!empty($_POST['dak_event_arranger_name'])) $dak_event_meta['dak_event_arranger_name'] = $_POST['dak_event_arranger_name'];
+    if(!empty($_POST['dak_event_arranger_logo'])) $dak_event_meta['dak_event_arranger_logo'] = $_POST['dak_event_arranger_logo'];
+    if(!empty($_POST['dak_event_arranger_description'])) $dak_event_meta['dak_event_arranger_description'] = $_POST['dak_event_arranger_description'];
+    if(!empty($_POST['dak_event_festival_id'])) $dak_event_meta['dak_event_festival_id'] = $_POST['dak_event_festival_id'];
+    if(!empty($_POST['dak_event_primary_picture'])) $dak_event_meta['dak_event_primary_picture'] = $_POST['dak_event_primary_picture'];
+    if(!empty($_POST['dak_event_covercharge'])) $dak_event_meta['dak_event_covercharge'] = $_POST['dak_event_covercharge'];
+    if(!empty($_POST['dak_event_age_limit'])) $dak_event_meta['dak_event_age_limit'] = $_POST['dak_event_age_limit'];
+    if(!empty($_POST['dak_event_created_at'])) $dak_event_meta['dak_event_created_at'] = $_POST['dak_event_created_at'];
+    if(!empty($_POST['dak_event_updated_at'])) $dak_event_meta['dak_event_updated_at'] = $_POST['dak_event_updated_at'];
+    if(!empty($_POST['dak_event_arranger'])) $dak_event_meta['dak_event_arranger'] = $_POST['dak_event_arranger'];
+    if(!empty($_POST['dak_event_categories'])) $dak_event_meta['dak_event_categories'] = $_POST['dak_event_categories'];
+    if(!empty($_POST['dak_event_festival'])) $dak_event_meta['dak_event_festival'] = $_POST['dak_event_festival'];
 
     // Add values of $events_meta as custom fields
     foreach ($dak_event_meta as $key => $value) { // Cycle through the $events_meta array!
@@ -437,6 +477,6 @@ function dak_event_save_post_meta($post_id, $post) {
         if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
     }
 }
-add_action('save_post', 'dak_event_save_post_meta', 1, 2); // save the custom fields
+
 
 ?>
--- dak_event_xmlrpc.php
+++ dak_event_xmlrpc.php
@@ -5,67 +5,123 @@
  * Author: Lisa Halvorsen, Snorre Davøen, Robin G. Aaberg
  * Version: 0.0000003
  */
+require_once('eventsCalendarClient.php');
+
+
     // Add method names here
-    $dak_event_xmlrpc_methods = array( 'dak_event.update_event' => 'dak_event_update_event',
-'dak_event.delete_event' => 'dak_event_delete_event',
-    );
+$dak_event_xmlrpc_methods = array( 'dak_event_ping' => 'dak_event_ping');
+$apiUrl = 'http://localhost:8888/kvarteret_symfony_events/web';
+$cacheType = 0;
+
+
 
 /*
  *  Adding xml-rpc methods
  */     
 
 function dak_event_add_xmlrpc_methods($methods) {
-    global $dak_event_xmlrpc_methods;
+    error_log("Add xmlrpc methods");
+    
+    $methods['dak_event_ping'] = 'dak_event_ping';
 
-    foreach ($dak_event_xmlrpc_methods as $xmlrpc_method => $php_method) {
-        $methods[$xmlrpc_method] = $php_method;
-    }
+    //global $dak_event_xmlrpc_methods;
+
+    // foreach ($dak_event_xmlrpc_methods as $xmlrpc_method => $php_method) {
+    //     $methods[$xmlrpc_method] = $php_method;
+    // }
+    //error_log(print_r($_POST, true));
 
     return $methods;
 
 }
 
     
-    function dak_event_update_event($args) {
-        global $wp_xmlrpc_server;
-        $wp_xmlrpc_server->escape( $args );
-        # xmlrp
-        
-        $blog_id  = $args[0];
-        $username = $args[1];
-        $password = $args[2];
+function dak_event_ping($args) {
+    error_log("Running dak_event_ping");
+    global $wp_xmlrpc_server;
+    $wp_xmlrpc_server->escape( $args );
+    # xmlrp
+    
+    $username = $args[0];
+    $password = $args[1];
 
-        if ( ! $user = $wp_xmlrpc_server->login( $username, $password ) )
-            return $wp_xmlrpc_server->error;
+    if ( ! $user = $wp_xmlrpc_server->login( $username, $password ) )
+        return $wp_xmlrpc_server->error;
 
-        /*
-         *  Functionality if authenticated and authorized:
-         */
-        
-        #Send me the Codez here
+    /*
+     *  Functionality if authenticated and authorized:
+     */
 
+    $type = $args[2];
+    $arrangement = $args[3];
+    $id = $args[4];
+
+    if($tarrangement == 'event') {
+        if($type == 'update') {
+            dak_event_updateEvent($id);
+        } else if($type == 'delete') {
+            dak_event_deleteEvent($id);
+        }
+    }
+}
+
+function dak_event_updateEvent($id) {
+
+    global $apiUrl, $cacheType;
+
+    $client = new eventsCalendarClient($apiUrl, null, $cacheType);
+    $response = $client->event($id);
+    $eventData = $response->data[0];
+    # Check if post already exist
+    $posts = get_posts(
+        array(
+            'meta_key' => 'dak_event_id',
+            'meta_value' => $id,
+            'meta_compare' => '==',
+            'post_type' => 'dak_event' 
+        )
+    );
+    $post_to_insert = array();
+    $post_id;
+    foreach($posts as $post) {
+       $post_id = $post->id;
+    }
+    # Default wp post fields
+    $post_to_insert['ID'] = $post_id;
+    $post_to_insert['title'] = '';
+    $post_to_insert['content'] = '';
+    $post_to_insert['thumbnail'] = '';
+
+    $post_id = wp_insert_post($post_to_insert);
+
+    #Dak event meta-fields
+    $meta_data_array = array();
+    add_meta_to_post_array($eventData, $meta_data_array);
+
+    foreach($post_to_insert as $key => $value) {
+        update_post_meta($post_id, $key, $value);
     }
 
     
-    function dak_event_delete_event($args) {
-        global $wp_xmlrpc_server;
-        $wp_xmlrpc_server->escape( $args );
-        # xmlrp
-        
-        $blog_id  = $args[0];
-        $username = $args[1];
-        $password = $args[2];
+    //$category_array = ();
+    //wp_set_object_terms($post_id, $categories, 'dak_event', true);
+}
 
-        if ( ! $user = $wp_xmlrpc_server->login( $username, $password ) )
-            return $wp_xmlrpc_server->error;
-
-        /*
-         *  Functionality if authenticated and authorized:
-         */
-        
-        #Send me the Codez here
-
+function add_meta_to_post_array($object, $array, $prepend='') {
+    foreach($object as $attrib => $value) {
+        if(!is_object($value)) {
+            $array[$prepend.$attrib] = $value;
+        } elseif (is_array($object)) {
+            # Nothing to do here
+        } else {
+            add_meta_to_post_array($value, $array, $attrib);
+        }
     }
+}
+
+function dak_event_deleteEvent($id) {
+
+}
 
 
 ?>
