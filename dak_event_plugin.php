bbd66966b8e3187c3b659ee5ff8b86d67b9a225d
Endre Oma <eoma@kvarteret.no>
2013-02-05 18:02:26 +0100

    Disable require of taxonomy as it doesn't exist right now

--- dak_event_plugin.php
+++ dak_event_plugin.php
@@ -10,7 +10,7 @@ $post_type_namespace = "dak_event";
 # Import plugin php modules
 require_once('dak_event_post_type.php');
 require_once('dak_event_xmlrpc.php');
-require_once('dak_event_register_taxonomy.php');
+//require_once('dak_event_register_taxonomy.php');
 
 # Set up hooks
 error_log("Setting up plugin");
@@ -18,7 +18,7 @@ error_log("Setting up plugin");
 // Adding xml-rpc methods
 add_filter( 'xmlrpc_methods', 'dak_event_add_xmlrpc_methods');
 add_action('init', 'dak_event_create_post_type');
-add_action('init', 'dak_event_register_taxonomy');
+//add_action('init', 'dak_event_register_taxonomy');
 add_action('save_post', 'dak_event_save_post_meta', 1, 2);
 
 
